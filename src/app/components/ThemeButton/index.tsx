3a9d1bfe4864cedaff8137f1d93bfeeafb201537
oiviana <vianalucas80@gmail.com>
2024-04-19 14:06:41 -0300

    feat: toggle theme logic

--- src/app/components/ThemeButton/index.tsx
+++ src/app/components/ThemeButton/index.tsx
@@ -1,19 +1,34 @@
 "use client";
-import { useState } from "react";
+
+interface ThemeButtonProps{
+
+}
+
+import { useState, useEffect } from "react";
 
 export default function ThemeButton() {
-  const [theme, setTheme] = useState(false);
-  console.log(theme);
-  const toogleTheme = () => {
-    setTheme(!theme);
-  };
+  const [themeDark, setThemeDark] = useState(true);
+
+  useEffect(()=>{
+    const theme = localStorage.getItem("theme")
+    if(theme === 'dark') setThemeDark(true)
+  },[])
+
+  useEffect(()=>{
+    if(themeDark){
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('theme','dark')
+    }else{
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('theme','light')
+    }
+  },[themeDark])
 
   return (
     <button
       className="absolute w-12 h-12 bottom-16 right-16 bg-orange-500 rounded-full text-slate-50"
-      onClick={toogleTheme}
+      onClick={()=> setThemeDark(!themeDark)}
     >
-      oii
     </button>
   );
 }
