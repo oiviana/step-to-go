eedfa6f69a1b6f68ba4573b5f5bad0049d1b5259
oiviana <vianalucas80@gmail.com>
2024-04-19 23:37:30 -0300

    refactor: remove unused elements

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -6,16 +6,9 @@ import ThemeButton from "../ThemeButton";
 export default function Header() {
   return (
     <>
-      <header className="w-100 flex bg-v-white-900 dark:bg-v-dark-500 h-20 ">
-        <div className="flex">
-          <section>
-            <Link href="/" className="font-bold text-xl text-gray-900 dark:text-slate-100">Logo</Link>
-          </section>
-          <section className="flex">
-            <Navbar />
-            <ThemeButton />
-          </section>
-        </div>
+      <header className="w-100 flex bg-v-white-900 dark:bg-v-dark-500 h-20 "> 
+          <Navbar />
+          <ThemeButton />
       </header>
     </>
   );
