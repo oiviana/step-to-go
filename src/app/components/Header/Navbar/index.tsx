0c2ebc3b5e5d56365f2d3766acda173df15fecd4
oiviana <vianalucas80@gmail.com>
2024-04-20 00:10:22 -0300

    style: dark and light mode settings

--- src/app/components/Header/Navbar/index.tsx
+++ src/app/components/Header/Navbar/index.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
     <nav className="flex w-full">
-      <div className="flex w-full justify-between items-center px-2">
+      <div className="flex w-full justify-between items-center px-4">
         <Link
           href="/"
           className="font-bold text-xl text-gray-900 dark:text-slate-100"
@@ -19,19 +19,19 @@ export default function Navbar() {
           className="flex md:hidden"
           onClick={() => setShowMobileMenu(true)}
         >
-          <GiHamburgerMenu size={30} className="text-v-dark-500 dark:text-gray-50" />
+          <GiHamburgerMenu size={30} className="text-v-dark-500 dark:text-v-white-500" />
         </button>
       </div>
 
       <ul
         className={`${
           showMobileMenu ? "fixed" : "hidden"
-        } md:hidden inset-y-0 left-0 z-10 w-full overflow-y-auto bg-v-white-900 dark:bg-v-dark-500`}
+        } md:hidden inset-y-0 left-0 z-10 w-full overflow-y-auto bg-v-white-900 dark:bg-v-dark-500 flex flex-col gap-5 text-v-dark-500 dark:text-v-white-500 text-base p-3`}
       >
         <div className="flex w-full justify-between p-2 mt-3">
           <Link
             href="/"
-            className="font-bold text-xl text-gray-900 dark:text-slate-100"
+            className="font-bold text-xl text-gray-900 dark:text-v-white-500"
           >
             Logo
           </Link>
@@ -39,7 +39,7 @@ export default function Navbar() {
             className="flex md:hidden"
             onClick={() => setShowMobileMenu(false)}
           >
-            <IoClose size={40} className="text-v-dark-500 dark:text-gray-50" />
+            <IoClose size={40} className="text-v-dark-500 dark:text-v-white-500" />
           </button>
         </div>
         <li>Sobre mim</li>
