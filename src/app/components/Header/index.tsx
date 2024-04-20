9b75d8f2fc0c6fd344b0b33c9201d1277c693213
oiviana <vianalucas80@gmail.com>
2024-04-20 14:26:26 -0300

    feat: header layout desktop

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,13 +2,29 @@ import { useState } from "react";
 import Navbar from "./Navbar";
 import Link from "next/link";
 import ThemeButton from "../ThemeButton";
+import { FaGithub } from "react-icons/fa";
+import { IoLanguage } from "react-icons/io5";
 
 export default function Header() {
   return (
     <>
-      <header className="w-100 flex bg-v-white-900 dark:bg-v-dark-500 h-20 "> 
+      <header className="w-100 flex flex-col bg-v-white-900 dark:bg-v-dark-500 px-2">
+        <div className="w-full justify-between ">
           <Navbar />
+        </div>
+        <div className="justify-end  hidden md:flex w-full max-w-[1440px] mx-auto gap-10 py-2">
+          <Link href="/">
+            <FaGithub 
+            className="text-v-dark-900 dark:text-v-white-500"
+            size={30}/>
+          </Link>
+          <Link href="/">
+            <IoLanguage 
+            className="text-v-dark-900 dark:text-v-white-500"
+            size={30}/>
+          </Link>
           <ThemeButton />
+        </div>
       </header>
     </>
   );
