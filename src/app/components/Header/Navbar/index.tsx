3eff8b3cd01c516fc475b4abc47f3b51f288d47f
oiviana <vianalucas80@gmail.com>
2024-04-19 23:37:47 -0300

    feat: initial responsive menu logic

--- src/app/components/Header/Navbar/index.tsx
+++ src/app/components/Header/Navbar/index.tsx
@@ -1,11 +1,51 @@
+"use client";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+import Link from "next/link";
+
 export default function Navbar() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
-    <nav>
-      <ul className="flex">
-        <li>Oi</li>
-        <li>Oi</li>
-        <li>Oi</li>
-        <li>Oi</li>
+    <nav className="flex w-full">
+      <div className="flex w-full justify-between items-center px-2">
+        <Link
+          href="/"
+          className="font-bold text-xl text-gray-900 dark:text-slate-100"
+        >
+          Logo
+        </Link>
+        <button
+          className="flex md:hidden"
+          onClick={() => setShowMobileMenu(true)}
+        >
+          <GiHamburgerMenu size={30} className="text-v-dark-500 dark:text-gray-50" />
+        </button>
+      </div>
+
+      <ul
+        className={`${
+          showMobileMenu ? "fixed" : "hidden"
+        } md:hidden inset-y-0 left-0 z-10 w-full overflow-y-auto bg-v-white-900 dark:bg-v-dark-500`}
+      >
+        <div className="flex w-full justify-between p-2 mt-3">
+          <Link
+            href="/"
+            className="font-bold text-xl text-gray-900 dark:text-slate-100"
+          >
+            Logo
+          </Link>
+          <button
+            className="flex md:hidden"
+            onClick={() => setShowMobileMenu(false)}
+          >
+            <IoClose size={40} className="text-v-dark-500 dark:text-gray-50" />
+          </button>
+        </div>
+        <li>Sobre mim</li>
+        <li>Profissional</li>
+        <li>Acadêmico</li>
+        <li>Projetos</li>
       </ul>
     </nav>
   );
