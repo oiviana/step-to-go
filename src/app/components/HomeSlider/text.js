c8883d3c039d1bd78b0e364d6e255b805d3180dd
oiviana <vianalucas80@gmail.com>
2024-04-22 14:23:49 -0300

    test: slider content mock

---
+++ src/app/components/HomeSlider/text.js
@@ -0,0 +1,5 @@
+export const texts = [
+  "Inteligência é a habilidade de se adaptar às mudanças",
+  "Um novo projeto com uma proposta de potencializar o aprendizado no mundo da programação!",
+  "Conheça meus projetos e minha proposta para ajudar pessoas que buscam se introduzir a esse mercado!",
+];
