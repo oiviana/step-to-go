5a68f3625a72ad572179e87798f539dba6126f47
oiviana <vianalucas80@gmail.com>
2024-04-23 16:10:58 -0300

    feat: calling time line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/";
 import HomeSlider from "./components/HomeSlider/";
 import Hero from "./components/Hero/";
-import LayoutSection from "./components/LayoutSection";
+import TimeLineCard from "./components/TimeLine/TimeLineCard";
 
 export default function Home() {
   return (
@@ -10,7 +10,8 @@ export default function Home() {
       <Header />
       <HomeSlider />
       <Hero />
-      <Footer />
+      <TimeLineCard/>
+
     </>
   );
 }
