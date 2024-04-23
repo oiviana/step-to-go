19f53d3dbe25c2d970034f7c6f27b39e47d5e2b1
oiviana <vianalucas80@gmail.com>
2024-04-23 15:25:50 -0300

    fix: fadeIn effect

--- src/app/components/HomeSlider/index.tsx
+++ src/app/components/HomeSlider/index.tsx
@@ -6,7 +6,6 @@ import LayoutSection from "../LayoutSection";
 export default function HomeSlider() {
   const [currentText, setCurrentText] = useState(0);
   const [fade, setFade] = useState(false);
-  console.log("Renderizou");
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -23,17 +22,14 @@ export default function HomeSlider() {
   return (
     <LayoutSection>
       <ul className="flex items-center text-2xl lg:text-5xl leading-[3rem] lg:leading-[4rem] font-bold min-h-[28rem] lg:min-h-[50rem]">
-        {texts.map((text, index) => (
-          <h1
-            key={index}
-            className={`transition-opacity ease-in-out duration-1000  ${
-              index === currentText && fade ? "opacity-0" : "opacity-100"
-            }`}
-            style={{ display: index === currentText ? "flex" : "none" }}
-          >
-            {text}
-          </h1>
-        ))}
+        <h1
+          className={`transition-opacity ease-in-out duration-1000 ${
+            fade ? "opacity-0" : "opacity-100"
+          }`}
+          style={{ display: "flex" }}
+        >
+          {texts[currentText]}
+        </h1>
       </ul>
     </LayoutSection>
   );
