b3f182b91bb8358df23a8e657dfaa4e8de5c4baf
oiviana <vianalucas80@gmail.com>
2024-04-24 17:08:50 -0300

    fix: layout section type

--- src/app/components/LayoutSection/index.tsx
+++ src/app/components/LayoutSection/index.tsx
@@ -1,4 +1,10 @@
-export default function LayoutSection({ children }) {
+import { ReactNode } from "react";
+
+interface LayoutSectionProps {
+  children: ReactNode;
+}
+
+export default function LayoutSection({ children }:LayoutSectionProps) {
   return (
     <section className="bg-v-white-300 dark:bg-v-dark-500 min-h-24 w-full py-14 px-2 text-v-dark-900 dark:text-v-white-500">
       <div className="w-full max-w-[1440px] mx-auto overflow-hidden">{children}</div>
