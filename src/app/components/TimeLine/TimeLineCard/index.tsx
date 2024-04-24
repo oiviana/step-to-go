80f5f4fe4beb36b94c6023598afa3c3558327579
oiviana <vianalucas80@gmail.com>
2024-04-24 16:46:12 -0300

    style; timeline grid

--- src/app/components/TimeLine/TimeLineCard/index.tsx
+++ src/app/components/TimeLine/TimeLineCard/index.tsx
@@ -11,7 +11,7 @@ export default function TimeLineCard({event, eventDate}:TimeLineCardProps) {
   return (
 
     
-        <div className="w-[10.5rem] md:w-[14rem] lg:w-[18rem] h-fit mb-6 ">
+        <div className="w-[10.5rem] md:w-[14rem] lg:w-[18rem] h-fit mb-7 ">
           <div className="relative">
             <div className="rounded-full w-3 h-3 bg-v-dark-900 dark:bg-v-white-500 absolute top-[-4px] left-[148px] md:left-[192px] lg:left-[240px]"></div>
             <div className=" w-full h-1 bg-v-dark-900 dark:bg-v-white-500 rounded "></div>
@@ -21,7 +21,7 @@ export default function TimeLineCard({event, eventDate}:TimeLineCardProps) {
               size={22}
               className="absolute right-0 bottom-[11.6rem] md:bottom-[13.6rem] lg:bottom-[15.6rem] text-white dark:text-v-dark-300"
             />
-            <label className="flex items-center justify-end text-xs lg:text-base gap-1 font-semibold mb-1">
+            <label className="flex items-center justify-end text-xs lg:text-base gap-1 font-semibold mb-2">
               <MdDateRange 
               size={18}/>
               {eventDate}
