1833d2c4055db147bfed5566689da435f0b071e6
oiviana <vianalucas80@gmail.com>
2024-04-24 17:10:22 -0300

    style: section padding

--- src/app/components/LayoutSection/index.tsx
+++ src/app/components/LayoutSection/index.tsx
@@ -6,7 +6,7 @@ interface LayoutSectionProps {
 
 export default function LayoutSection({ children }:LayoutSectionProps) {
   return (
-    <section className="bg-v-white-300 dark:bg-v-dark-500 min-h-24 w-full py-14 px-2 text-v-dark-900 dark:text-v-white-500">
+    <section className="bg-v-white-300 dark:bg-v-dark-500 min-h-24 w-full py-16 px-2 text-v-dark-900 dark:text-v-white-500">
       <div className="w-full max-w-[1440px] mx-auto overflow-hidden">{children}</div>
     </section>
   );
