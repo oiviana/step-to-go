d32b5c465aa6ae36c47e45e4bb14fc1d78a028ab
oiviana <vianalucas80@gmail.com>
2024-04-25 14:45:57 -0300

    feat: projects data

---
+++ src/app/components/MyProjects/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    "title": "fev/2016",
+    "subtitle":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "description":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "stack":["Next, bla, ble"],
+    "hasDeploy": false
+
+  },
+  {
+    "title": "fev/2016",
+    "subtitle":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "description":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "stack":["Next, bla, ble"],
+    "hasDeploy": true
+
+  },
+  {
+    "title": "fev/2016",
+    "subtitle":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "description":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "stack":["Next, bla, ble"],
+    "hasDeploy": true
+
+  },
+  {
+    "title": "fev/2016",
+    "subtitle":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "description":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "stack":["Next, bla, ble"],
+    "hasDeploy": false
+
+  },
+  {
+    "title": "fev/2016",
+    "subtitle":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "description":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "stack":["Next, bla, ble"],
+    "hasDeploy": true
+
+  },
+  {
+    "title": "fev/2016",
+    "subtitle":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "description":"Iniciei meu curso técnico e tive meu primeiro contato com programação meu curso técnico e tive meu primeiro contato com programação.",
+    "stack":["Next, bla, ble"],
+    "hasDeploy": true
+
+  },
+  ];
+  
