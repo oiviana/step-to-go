ceccdafa35506a0190e939bdcff3555fd688c443
oiviana <vianalucas80@gmail.com>
2024-04-25 22:53:28 -0300

    style: title section position

--- src/app/components/TitleSection/index.tsx
+++ src/app/components/TitleSection/index.tsx
@@ -1,6 +1,6 @@
 export default function TitleSection({ title }: { title: string }) {
   return (
-    <h2 className="text-2xl lg:text-4xl font-bold py-5 lg:pb-16 sm:text-center lg:text-left">
+    <h2 className="text-2xl lg:text-4xl font-bold py-5 lg:pb-16 text-center lg:text-left">
       {title}
     </h2>
   );
