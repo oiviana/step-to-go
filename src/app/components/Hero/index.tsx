3002789d435bac29343da5cc1fa6ce4c4ce3aa84
oiviana <vianalucas80@gmail.com>
2024-04-27 15:43:49 -0300

    refactor: display CV on mobile

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <LayoutSection>
       <div className="relative">
-        <TitleSection title="Prazer, me chamo Lucas Viana!"/>
+        <TitleSection title="Prazer, me chamo Lucas Viana!" />
         <div className="flex flex-col-reverse lg:flex-row lg:justify-between">
           <div className="lg: max-w-[40rem] mt-[2rem] lg:mt-0 sm:mx-auto lg:mx-0">
             <p className="leading-9">
@@ -16,24 +16,29 @@ export default function Hero() {
               mercado, aplicando meu conhecimento e habilidades para solucionar
               desafios em projetos diversos.
             </p>
-            <h3 className="text-xl py-6">
-                Tecnologias que utilizo:
-            </h3>
-            <TechnologySlider/>
+            <h3 className="text-xl py-6">Tecnologias que utilizo:</h3>
+            <TechnologySlider />
           </div>
-          <div className="lg:absolute top-7 right-0 sm:mx-auto lg:mx-0">
-            <Image
-              src="/hero.png"
-              width={400}
-              height={400}
-              alt="Lucas Viana"
-              className="rounded-full"
-            />
+          <div>
+            <div className="lg:absolute top-7 right-0 flex justify-center lg:mx-0">
+              <Image
+                src="/hero.png"
+                width={400}
+                height={400}
+                alt="Lucas Viana"
+                className="rounded-full"
+              />
+            </div>
+            <div className="bottom-[15rem] mt-2 lg:flex items-end lg:h-[378px] min-w-[300px] flex justify-center lg:justify-normal">
+              <a
+                href="https://drive.google.com/file/d/1JmXwOsGQTlhPGan3HOQivF8264nA1QBw/view?usp=sharing"
+                target="_blank"
+                className="p-5 h-[60px] flex max-w-[220px] justify-center rounded-md lg:absolute shadow-md font-semibold bg-v-dark-900 dark:bg-v-white-500 text-v-white-300 dark:text-v-dark-bold"
+              >
+                Veja meu currículo
+              </a>
+            </div>
           </div>
-          <div className="bottom-[15rem] hidden lg:flex items-end lg:h-[378px] min-w-[300px]">
-          <button className="p-5 h-[60px] rounded-md absolute shadow-md font-semibold bg-v-dark-900 dark:bg-v-white-500 text-v-white-300 dark:text-v-dark-bold">Veja meu currículo</button>
-          </div>
-     
         </div>
       </div>
     </LayoutSection>
