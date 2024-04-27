4d1db31ff641d64cb4dfe4118ef92d371d01d461
oiviana <vianalucas80@gmail.com>
2024-04-26 22:25:11 -0300

    test: react hook form implementation

--- src/app/components/Contact/EmailForm/index.tsx
+++ src/app/components/Contact/EmailForm/index.tsx
@@ -1,16 +1,34 @@
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface EmailFormProps {
+  name: string
+  email: string
+  message: string
+}
+
 export default function EmailForm() {
+  const { handleSubmit, register } = useForm<EmailFormProps>();
+
+  const handleSendEmail: SubmitHandler<EmailFormProps> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="max-w-[480px] flex flex-col gap-8  py-12">
+    <form
+      className="max-w-[480px] flex flex-col gap-8  py-12"
+      onSubmit={handleSubmit(handleSendEmail)}
+    >
       <fieldset className="relative border-2 bg-white  dark:bg-v-dark-300 border-v-white-900 dark:border-slate-400 rounded-md group">
         <legend className="text-base md:text-lg font-bold  ml-4 px-3 tracking-wider !leading-[14px]">
           Nome:
         </legend>
         <input
           type="text"
-          name="name"
+          {...register("name", { required: true, maxLength: 20 })}
           id="name"
           required
-          className="p-2 rounded-md w-full bg-transparent outline-0"
+          className="p-2 rounded-md w-full !bg-transparent outline-0"
         />
       </fieldset>
       <fieldset className="relative border-2 bg-white  dark:bg-v-dark-300 border-v-white-900 dark:border-slate-400 rounded-md ">
@@ -19,7 +37,7 @@ export default function EmailForm() {
         </legend>
         <input
           type="email"
-          name="email"
+          {...register("email", { required: true })}
           id="name"
           required
           className="p-2 rounded-md w-full bg-transparent outline-0"
@@ -30,9 +48,9 @@ export default function EmailForm() {
           Mensagem:
         </legend>
         <textarea
-          name="message"
+          {...register("message", { required: true, maxLength: 20 })}
           id="message"
-          rows="10"
+          rows={10}
           required
           className="p-2 rounded-md rounded-ee-none w-full bg-transparent outline-0"
         ></textarea>
@@ -40,7 +58,7 @@ export default function EmailForm() {
       <input
         type="submit"
         value="Enviar"
-        className="p-4 h-[50px] flex items-center justify-center w-full max-w-[180px] mx-auto leading-3 lg:mx-0 rounded-md shadow-md font-semibold bg-v-dark-900 dark:bg-v-white-500 text-v-white-300 dark:text-v-dark-bold"
+        className="p-4 h-[50px] cursor-pointer flex items-center justify-center w-full max-w-[180px] mx-auto leading-3 lg:mx-0 rounded-md shadow-md font-semibold bg-v-dark-900 dark:bg-v-white-500 text-v-white-300 dark:text-v-dark-bold"
       />
     </form>
   );
