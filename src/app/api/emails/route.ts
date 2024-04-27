e5fc241fccee56c25becd98c65982e777aba1961
oiviana <vianalucas80@gmail.com>
2024-04-27 02:06:33 -0300

    test: req params for email

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -1,17 +1,19 @@
 import { EmailTemplate } from "@/app/components/EmailTest/email-template";
 import { NextResponse } from "next/server";
+import type { NextApiRequest} from 'next'
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.EMAIL_API_KEY);
 
-export async function POST() {
+export async function POST(req: NextApiRequest,) {
   try {
+    const { name, email, message } = req.body;
     const {data} = await resend.emails.send({
       from: `Enviado por: <onboarding@resend.dev>`,
       to: ["vianalucas80@gmail.com"],
-      subject: "teste",
+      subject: `Email enviado por ${email}`,
       text:"",
-      react: EmailTemplate({ firstName: "Usuáriooo" }),   
+      react: EmailTemplate({ firstName: name, email, message}),   
     });
 
     return NextResponse.json({data})
