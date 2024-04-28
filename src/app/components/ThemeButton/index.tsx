56f45c30f071a137bcf4598e2583644f5a90dd08
oiviana <vianalucas80@gmail.com>
2024-04-28 15:49:08 -0300

    feat: links at navbar

--- src/app/components/ThemeButton/index.tsx
+++ src/app/components/ThemeButton/index.tsx
@@ -26,19 +26,19 @@ export default function ThemeButton() {
 
   return (
     <button
-      className="relative hidden md:flex w-16 h-8 rounded-full p-1 bg-white dark:bg-v-dark-bold shadow"
+      className="relative  md:flex w-16 h-8 rounded-full p-1 bg-white dark:bg-v-dark-bold shadow"
       onClick={() => setThemeDark(!themeDark)}
     >
       <FaMoon size={22} 
       className=" text-v-dark-500 mt-[1px]"/>
       <div
-        className="absolute bg-v-white-900 dark:bg-v-dark-300 w-6 h-6 rounded-full shadow-md transform transition-transform duration-200"
+        className="absolute top-[4px] bg-v-white-900 dark:bg-v-dark-300 w-6 h-6 rounded-full shadow-md transform transition-transform duration-200 z-30"
         style={themeDark ? { left: "4px" } : { right: "4px" }}
       >
       </div>
       <FaRegSun
       size={22}
-      className="ml-auto text-gray-50 mt-[1px]"/>
+      className="ml-auto text-gray-50 absolute right-[5px] top-[5px]"/>
     </button>
   );
 }
