c4e5ab623b8feeea6dc1f957a785676c2d60298e
oiviana <vianalucas80@gmail.com>
2024-04-28 15:49:42 -0300

    refactor: id prop on title

--- src/app/components/TitleSection/index.tsx
+++ src/app/components/TitleSection/index.tsx
@@ -1,6 +1,6 @@
-export default function TitleSection({ title }: { title: string }) {
+export default function TitleSection({ title, id }: { title: string, id: string }) {
   return (
-    <h2 className="text-2xl lg:text-4xl font-bold py-5 lg:pb-16 text-center lg:text-left">
+    <h2 className="text-2xl lg:text-4xl font-bold py-5 lg:pb-16 text-center lg:text-left" id={id}>
       {title}
     </h2>
   );
