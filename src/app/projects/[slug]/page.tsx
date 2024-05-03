88db533f5ed83fab192ba7965b0a73ac7acc8e8f
oiviana <vianalucas80@gmail.com>
2024-05-02 22:43:38 -0300

    fix: slider height

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -38,27 +38,34 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
         </h2>
       </header>
       <section className="mb-5 ">
-        <div className="h-[400px] lg:h-[550px] w-full">
-         
+        <div className="h-[400px] lg:h-[620px] w-full">
           <ProjectSlider images={projectBySlug.images} />
         </div>
       </section>
       <section>
         <div className="flex flex-col shadow-lg py-6 md:p-7 gap-4 bg-v-white-900 min-h-[450px] w-full rounded-t-md bg-gradient-to-b dark:from-v-dark-700 from-10% dark:via-v-dark-900 via-40% dark:to-v-dark-bold to-80% px-2 dark:text-v-white-300">
-          <div>
-          <h1 className="text-lg md:text-3xl font-bold text-center lg:text-left">
-            Projeto lindo que eu fiz
-          </h1>
-          <button>
-            <FaCode size={18}/>
-            <a href="">Código</a>
-            </button>
-          <button>Deploy</button>
+          <div className="flex justify-center md:justify-between flex-col md:flex-row">
+            <h1 className="text-xl md:text-3xl font-bold text-center lg:text-left">
+            {projectBySlug.title}
+            </h1>
+            <div className="flex gap-4 justify-center mt-4 md:mt-0">
+              <button className="flex bg-v-dark-bold dark:bg-v-white-500 dark:text-v-dark-bold text-v-white-300 text-lg font-semibold py-2 px-3 gap-3 items-center rounded">
+                <FaCode size={18} />
+                <a href={projectBySlug.codeUrl}>Código</a>
+              </button>
+              <button className="flex bg-v-dark-bold dark:bg-v-white-500 dark:text-v-dark-bold text-v-white-300 text-lg font-semibold py-2 px-3 gap-3 items-center rounded">
+                <VscPreview size={18} />
+                <a href={projectBySlug.deployUrl}>Deploy</a>
+              </button>
+            </div>
           </div>
-          <span className="text-slate-600 dark:text-slate-500">Tecnologias: {stackList}</span>
-       
+          <span className="text-slate-600 dark:text-slate-500">
+            Tecnologias: {stackList}
+          </span>
+
           <article className="mt-8 !leading-[40px] text-sm md:text-md !font-normal">
-            <b>Descrição: </b>{projectBySlug.description}
+            <b>Descrição: </b>
+            {projectBySlug.description}
           </article>
         </div>
       </section>
