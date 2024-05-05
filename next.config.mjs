6f2d4e16575b18cff2c522af7c1e861a9174337b
oiviana <vianalucas80@gmail.com>
2024-05-05 16:16:13 -0300

    config: next config with IntL

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -11,5 +15,5 @@ const nextConfig = {
     ],
   },
 };
-
-export default nextConfig;
+ 
+export default withNextIntl(nextConfig);
