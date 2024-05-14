afea796ba804a46530e85668058a01a6c31059a1
oiviana <vianalucas80@gmail.com>
2024-05-14 13:15:43 -0300

    refactor: email route

---
+++ src/app/api/contactEmail/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse, NextRequest } from "next/server";
+import { EmailFormProps } from "@/components/Contact/EmailForm";
+import { mailOptions, transporter } from "@/services/nodemailer";
+
+export async function POST(req: NextRequest) {
+
+  try {
+    const body: EmailFormProps = await req.json();
+    console.log(body);
+
+    await transporter.sendMail({
+      ...mailOptions,
+      subject: `Você recebeu um email de ${body.name}`,
+      text: `${body.message}`,
+      html: `
+      <p>Name: ${body.name} </p>
+      <p>Email: ${body.email} </p>
+      <p>Message: ${body.message} </p>
+      `,
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (err) {
+    console.log("There's something wrong ", err);
+    return NextResponse.error();
+  }
+}
