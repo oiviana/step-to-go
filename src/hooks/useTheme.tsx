67448b8c63006b8e0ec5891d7fc5f171e5bb0110
oiviana <vianalucas80@gmail.com>
2024-05-14 16:00:54 -0300

    fix: prettier

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
 
 function useTheme(): [boolean, () => void] {
+  
   const getInitialTheme = () => {
-    if (typeof window !== 'undefined') { 
-      const theme = localStorage.getItem('theme');
-      return theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches);
+    if (typeof window !== "undefined") {
+      const theme = localStorage.getItem("theme");
+      return (
+        theme === "dark" ||
+        (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches)
+      );
     }
-    return false; 
+    return false;
   };
 
   const [themeDark, setThemeDark] = useState<boolean>(getInitialTheme);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') { 
-      const theme = localStorage.getItem('theme');
-      if (theme === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if (typeof window !== "undefined") {
+      const theme = localStorage.getItem("theme");
+      if (
+        theme === "dark" ||
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
         setThemeDark(true);
       }
     }
@@ -22,11 +29,11 @@ function useTheme(): [boolean, () => void] {
 
   useEffect(() => {
     if (themeDark) {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
   }, [themeDark]);
 
