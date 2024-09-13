123e1541d5b27fb19ad4553475021ed3304ad86a
oiviana <vianalucas80@gmail.com>
2024-09-13 11:19:47 -0300

    refactor: cv url

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
             </div>
             <div className="bottom-[15rem] mt-2 lg:flex items-end lg:h-[378px] min-w-[300px] flex justify-center lg:justify-normal">
               <a
-                href="https://drive.google.com/file/d/1ECKbFSpiaXA81bCPgWU8F2RNxCV8Seq7/view?usp=drive_link"
+                href={process.env.CV_BUTTON_URL}
                 target="_blank"
                 className="w-[214px] p-5 h-[60px] flex  justify-center items-center rounded-md lg:absolute shadow-md font-semibold bg-v-dark-900 dark:bg-v-white-500 text-v-white-300 dark:text-v-dark-bold hover:bg-v-dark-500 hover:dark:bg-v-white-900 transition-colors"
               >
