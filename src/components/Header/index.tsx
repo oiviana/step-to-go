e9e24cffbba939fcc69f6094e515f5c6b8dee9c9
oiviana <vianalucas80@gmail.com>
2024-09-19 12:20:31 -0300

    test: new header width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,11 +6,11 @@ import TranslateButton from "../TranslateButton";
 export default function Header() {
   return (
     <>
-      <header className="w-100 flex flex-col bg-v-white-500 dark:bg-v-dark-700 px-2 shadow">
+      <header className="w-100 flex flex-col  bg-[#058689] px-2 shadow max-w-[1170px] mx-auto">
         <div className="w-full justify-between ">
           <Navbar />
         </div>
-        <div className="justify-end  hidden md:flex w-full max-w-[1440px] mx-auto gap-10 py-2">
+        <div className="justify-end  hidden md:flex w-full  mx-auto gap-10 py-2">
           <TranslateButton/>
           <ThemeButton />
         </div>
@@ -18,3 +18,5 @@ export default function Header() {
     </>
   );
 }
+
+// bg-[#2e7dbb]
