54898365fd05378fa506fb3d43f050cb788c1664
oiviana <vianalucas80@gmail.com>
2024-09-21 18:12:26 -0300

    test: footer and header positions

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link";
-import Logo from "../Logo";
-import NextLogo from "../Logo/NextLogo";
+// import Link from "next/link";
+// import Logo from "../Logo";
+// import NextLogo from "../Logo/NextLogo";
 import { useTranslations } from "next-intl";
 
 export default function Footer() {
   const translate = useTranslations("Footer");
 
   return (
-    <footer className="w-full bg-v-dark-test isolate ">
-      <div className="max-w-[1170px] flex flex-col mx-auto px-2 pt-[80px] text-v-dark-bold dark:text-v-white-300">
+    <footer className="w-full lg:fixed bottom-0">
+      <div className="max-w-[1170px] flex flex-col mx-auto px-2 pt-[20px] text-v-dark-bold dark:text-v-white-300 bg-slate-300">
         <div className="flex flex-col items-center gap-9 lg:gap-0 lg:flex-row lg:justify-between lg:items-start">
 
         </div>
