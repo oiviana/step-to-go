c5d38796be76270e2f1b15c2bc15ce595faef627
oiviana <vianalucas80@gmail.com>
2024-09-21 18:12:42 -0300

    test: sections navigations

--- src/components/Header/Navbar/DesktopItems/index.tsx
+++ src/components/Header/Navbar/DesktopItems/index.tsx
@@ -1,17 +1,26 @@
 "use client"
 
 import { useNavigationPage } from "@/utils/Providers";
+import { useState } from "react";
 
 export default function DesktopItems() {
+
     const {setPage} = useNavigationPage();
+    const [activeSection, setActiveSection] = useState("");
+
+    const handleSetSection = (section:string) => {
+        setPage(section)
+        setActiveSection(section)
+    }
+
   return (
     <ul className="hidden md:flex text-v-dark-500 dark:text-v-white-500 items-center gap-8 md:text-lg lg:gap-24">
-      <li className="hover:text-black hover:dark:text-v-white-900 transition-colors"
-      onClick={() => setPage("about")}>
+      <li className={`transition-colors cursor-pointer  ${activeSection === 'about' && 'text-v-green'}`}
+      onClick={() => handleSetSection("about")}>
         Sobre mim
       </li>
-      <li className="hover:text-black hover:dark:text-v-white-900 transition-colors"
-       onClick={() => setPage("projects")}>
+      <li className={`transition-colors cursor-pointer  ${activeSection === 'projects' && 'text-v-green'}`}
+       onClick={() => handleSetSection("projects")}>
         Projetos
       </li>
     </ul>
