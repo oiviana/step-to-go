a5e247b3818bf4a94672e7e49593d1ee5ed4d02a
oiviana <vianalucas80@gmail.com>
2024-09-22 17:28:11 -0300

    style: layout section

--- src/components/LayoutSection/index.tsx
+++ src/components/LayoutSection/index.tsx
@@ -6,7 +6,7 @@ interface LayoutSectionProps {
 
 export default function LayoutSection({ children }: LayoutSectionProps) {
   return (
-    <section className="section-scroll min-h-24 w-full px-2 min-[1170px]:px-0 lg:pb-[180px] text-v-dark-bold dark:text-v-white-300 max-w-[1170px] mx-auto  flex lg:overflow-scroll lg:overflow-x-hidden lg:h-[46rem]">
+    <section className="section-scroll min-h-24 w-full px-2 min-[1170px]:px-0 lg:pb-[180px] text-v-dark-bold dark:text-v-white-300 max-w-[1170px] mx-auto  flex lg:overflow-scroll lg:overflow-x-hidden lg:h-[46rem] pt-4">
       {children}
     </section>
   );
