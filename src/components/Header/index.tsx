e0af4fc1eb38a2ca21c05c5c5617b6a12ae472aa
oiviana <vianalucas80@gmail.com>
2024-09-23 11:49:26 -0300

    refactor: logo structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,31 +11,47 @@ import MobileHamburguer from "./MobileHamburguer";
 export default function Header() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
-    <header className="w-100 flex flex-col px-4 lg:px-0 w-full max-w-[1170px] mx-auto fixed lg:sticky py-4 lg:py-0 bg-v-dark-test">
-      <div className="w-full justify-between flex items-center">
-        <div className="flex w-full justify-between items-center lg:pt-5">
-          <Link
-            href="/"
-            className=" w-full md:max-w-44  flex gap-4"
-            aria-label="Logo do site"
-          >
-            <Logo />
-            <TextLogo />
-          </Link>
-          <button
-            className="flex md:hidden"
-            onClick={() => setShowMobileMenu(!showMobileMenu)}
-            aria-label="Abrir menu"
-          >
-            <MobileHamburguer openedMenu={showMobileMenu} />
-          </button>
+    <header className="w-100 flex flex-col px-4 lg:px-0 w-full max-w-[1170px] mx-auto fixed lg:sticky py-4 lg:py-0 bg-v-dark-test lg:h-[74px]">
+      <div className="flex items-center h-full">
+        <div className="w-full justify-between items-center h-full hidden lg:flex">
+          <div className="flex w-full justify-between items-center">
+            <Link
+              href="/"
+              className=" w-full md:max-w-44  flex gap-4"
+              aria-label="Logo do site"
+            >
+              <Logo width={100} height={53}/>
+              <TextLogo width={150} height={30}/>
+            </Link>
+          </div>
         </div>
         <Navbar
           showMobileMenu={showMobileMenu}
           setShowMenu={setShowMobileMenu}
         />
-           <TranslateButton />
-      </div>    
+        <div className="hidden lg:flex">
+          <TranslateButton />
+        </div>
+      </div>
+      <div className="w-full justify-between items-center h-full flex lg:hidden">
+        <div className="flex w-full justify-between items-center">
+          <Link
+            href="/"
+            className=" w-full md:max-w-44  flex gap-4"
+            aria-label="Logo do site"
+          >
+            <Logo width={80} height={42}/>
+            <TextLogo width={112} height={17}/>
+          </Link>
+        </div>
+        <button
+          className="flex md:hidden"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+          aria-label="Abrir menu"
+        >
+          <MobileHamburguer openedMenu={showMobileMenu} />
+        </button>
+      </div>
     </header>
   );
 }
