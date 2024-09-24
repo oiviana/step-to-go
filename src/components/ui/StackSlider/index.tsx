47bd066d0a2fe29581c6d4da91f38fccf923f4f0
oiviana <vianalucas80@gmail.com>
2024-09-24 18:01:43 -0300

    test: stack slider initial structure

---
+++ src/components/ui/StackSlider/index.tsx
@@ -0,0 +1,14 @@
+export default function StackSlider(){
+
+    return(
+        <div>
+            <ul className="flex mt-4 gap-5 w-full bg-slate-300 overflow-hidden">
+                <li className="bg-red-400 w-32 h-14">Teste</li>
+                <li className="bg-red-400 w-32 h-14">Teste</li>
+                <li className="bg-red-400 w-32 h-14">Teste</li>
+                <li className="bg-red-400 w-32 h-14">Teste</li>
+                <li className="bg-red-400 w-32 h-14">Teste</li>
+            </ul>
+        </div>
+    );
+}
