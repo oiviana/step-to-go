54beb39e5febe5d5befdc400d053a67723919123
oiviana <vianalucas80@gmail.com>
2024-09-24 22:36:14 -0300

    style: padding section at mobile

--- src/components/LayoutSection/index.tsx
+++ src/components/LayoutSection/index.tsx
@@ -6,7 +6,7 @@ interface LayoutSectionProps {
 
 export default function LayoutSection({ children }: LayoutSectionProps) {
   return (
-    <section className="section-scroll min-h-24 w-full px-2 lg:pt-20 min-[1170px]:px-0 lg:pb-[180px] text-v-white-300 max-w-[1170px] mx-auto flex flex-col lg:overflow-scroll lg:overflow-x-hidden lg:h-[46rem] mt-16 lg:mt-8">
+    <section className="section-scroll min-h-24 w-full px-2 lg:pt-20 min-[1170px]:px-0 lg:pb-[180px] text-v-white-300 max-w-[1170px] mx-auto flex flex-col lg:overflow-scroll lg:overflow-x-hidden lg:h-[46rem] mt-20 lg:mt-8">
       {children}
     </section>
   );
