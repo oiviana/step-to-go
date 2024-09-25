19b97ebf29ace0c0491c5f189ee8fa128d7d32ce
oiviana <vianalucas80@gmail.com>
2024-09-24 22:35:50 -0300

    feat: stack slider structure

--- src/components/ui/StackSlider/index.tsx
+++ src/components/ui/StackSlider/index.tsx
@@ -1,14 +1,20 @@
-export default function StackSlider(){
+import Image from "next/image";
+import { sliderMock } from "./mock";
 
-    return(
-        <div>
-            <ul className="flex mt-4 gap-5 w-full bg-slate-300 overflow-hidden">
-                <li className="bg-red-400 w-32 h-14">Teste</li>
-                <li className="bg-red-400 w-32 h-14">Teste</li>
-                <li className="bg-red-400 w-32 h-14">Teste</li>
-                <li className="bg-red-400 w-32 h-14">Teste</li>
-                <li className="bg-red-400 w-32 h-14">Teste</li>
-            </ul>
-        </div>
-    );
+export default function StackSlider() {
+  const sliderItems = sliderMock.map((item) => (
+    <li className="w-[200px] h-[65px] flex items-center flex-shrink-0 gap-3  font-bold lg:text-xl text-v-purple">
+      <Image src={item.techImg} alt={item.techName} width={65} height={65} />
+      <span>{item.techName}</span>
+    </li>
+  ));
+
+  return (
+    <div className="stack-slider-container relative w-full overflow-hidden flex">
+      <ul className="flex mt-4 gap-6 w-full  stack-slider">
+        {sliderItems}
+        {sliderItems}
+      </ul>
+    </div>
+  );
 }
---
+++ src/components/ui/StackSlider/mock.ts
@@ -0,0 +1,34 @@
+export const sliderMock = [
+    {
+      techImg: "/stack-slider/stack-slide-react.png",
+      techName:"React JS"
+    },
+    {
+        techImg: "/stack-slider/stack-slide-vtex.png",
+        techName:"VTEX IO"
+    },
+    {
+        techImg: "/stack-slider/stack-slide-react.png",
+        techName:"React JS"
+      },
+      {
+          techImg: "/stack-slider/stack-slide-docker.png",
+          techName:"Docker"
+      },
+      {
+        techImg: "/stack-slider/stack-slide-react.png",
+        techName:"React JS"
+      },
+      {
+          techImg: "/stack-slider/stack-slide-vtex.png",
+          techName:"VTEX IO"
+      },
+      {
+        techImg: "/stack-slider/stack-slide-react.png",
+        techName:"React JS"
+      },
+      {
+          techImg: "/stack-slider/stack-slide-vtex.png",
+          techName:"VTEX IO"
+      },
+]
