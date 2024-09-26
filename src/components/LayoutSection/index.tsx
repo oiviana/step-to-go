036154d94e86f9259d9432dbdb0f54b94d6e99a5
oiviana <vianalucas80@gmail.com>
2024-09-26 12:34:14 -0300

    fix: layout section adjustments

--- src/components/LayoutSection/index.tsx
+++ src/components/LayoutSection/index.tsx
@@ -23,14 +23,14 @@ export default function LayoutSection({ children }: LayoutSectionProps) {
 
       section.addEventListener('scroll', handleScroll);
 
-      // Cleanup the event listener on component unmount
       return () => {
         section.removeEventListener('scroll', handleScroll);
       };
     }
-  }, []); 
+  }, []);
+  
   return (
-    <section className="section-scroll min-h-24 w-full px-2 lg:pt-20 min-[1170px]:px-0 lg:pb-[180px] text-v-white-300 max-w-[1170px] mx-auto flex flex-col lg:overflow-scroll lg:overflow-x-hidden lg:h-[46rem] mt-20 lg:mt-8">
+    <section className="section-scroll min-h-24 w-full px-2 lg:pt-20 min-[1170px]:px-0 lg:pb-[180px] text-v-white-300 max-w-[1170px] mx-auto flex flex-col lg:overflow-scroll lg:overflow-x-hidden lg:h-[46rem] mt-40 lg:mt-8">
       {children}
     </section>
   );
