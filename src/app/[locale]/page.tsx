587f325a9ddb4521f35e55b08bd9804ceb86b82c
oiviana <vianalucas80@gmail.com>
2024-09-26 16:30:59 -0300

    refactor: main structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,27 +1,13 @@
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import HomeSlider from "../../components/HomeSlider";
-import Hero from "../../components/Hero";
-import TimeLine from "../../components/TimeLine";
-import MyProjects from "../../components/MyProjects";
-import Contact from "../../components/Contact";
-import { fetchProjects } from "@/contentful/myProjects";
-import { useLocale } from "next-intl";
 import Main from "@/components/Main";
 
-export default async function Home() {
-  const locale = useLocale();
-  const getProjects = await fetchProjects({ locale: locale });
+export default function Home() {
+
+
   return (
+    <>
     <Main/>
-    // <>
-    //   <Header />
-    //   {/* <HomeSlider /> */}
-    //   {/* <Hero />
-    //   <TimeLine /> */}
-    //   {/* <MyProjects myProjects={getProjects} />
-    //   <Contact /> */}
-    //   {/* <Footer /> */}
-    // </>
+
+    </>
+
   );
 }
