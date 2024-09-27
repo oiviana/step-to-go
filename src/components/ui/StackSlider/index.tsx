2b3026132d04bbb0a28785d2c39ab4a710a89004
oiviana <vianalucas80@gmail.com>
2024-09-26 21:05:34 -0300

    feat: new stack slider adjustments

--- src/components/ui/StackSlider/index.tsx
+++ src/components/ui/StackSlider/index.tsx
@@ -3,14 +3,14 @@ import { sliderMock } from "./mock";
 
 export default function StackSlider() {
   const sliderItems = sliderMock.map((item) => (
-    <li className="w-[200px] h-[65px] flex items-center flex-shrink-0 gap-3  font-bold lg:text-xl text-v-purple">
+    <li className="w-[200px] h-[105px] flex flex-col items-center flex-shrink-0 gap-3  font-bold lg:text-xl text-v-purple ">
       <Image src={item.techImg} alt={item.techName} width={65} height={65} />
       <span>{item.techName}</span>
     </li>
   ));
 
   return (
-    <div className="stack-slider-container relative w-full overflow-hidden flex">
+    <div className="stack-slider-container relative w-full overflow-hidden flex z-[-1]">
       <ul className="flex mt-4 gap-6 w-full  stack-slider">
         {sliderItems}
         {sliderItems}
--- src/components/ui/StackSlider/mock.ts
+++ src/components/ui/StackSlider/mock.ts
@@ -1,34 +1,44 @@
 export const sliderMock = [
-    {
-      techImg: "/stack-slider/stack-slide-react.png",
-      techName:"React JS"
-    },
-    {
-        techImg: "/stack-slider/stack-slide-vtex.png",
-        techName:"VTEX IO"
-    },
-    {
-        techImg: "/stack-slider/stack-slide-react.png",
-        techName:"React JS"
-      },
-      {
-          techImg: "/stack-slider/stack-slide-docker.png",
-          techName:"Docker"
-      },
-      {
-        techImg: "/stack-slider/stack-slide-react.png",
-        techName:"React JS"
-      },
-      {
-          techImg: "/stack-slider/stack-slide-vtex.png",
-          techName:"VTEX IO"
-      },
-      {
-        techImg: "/stack-slider/stack-slide-react.png",
-        techName:"React JS"
-      },
-      {
-          techImg: "/stack-slider/stack-slide-vtex.png",
-          techName:"VTEX IO"
-      },
-]
+  {
+    techImg: "/stack-slider/stack-slide-react.png",
+    techName: "React JS",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-vtex.png",
+    techName: "VTEX IO",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-ts.png",
+    techName: "Typescript",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-docker.png",
+    techName: "Docker",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-tailwind.png",
+    techName: "Tailwind CSS",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-deco.png",
+    techName: "Deco.cx",
+  },
+
+  {
+    techImg: "/stack-slider/stack-slide-jest.png",
+    techName: "Jest",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-post.png",
+    techName: "PostgreSQL",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-csharp.png",
+    techName: "C#",
+  },
+  {
+    techImg: "/stack-slider/stack-slide-git.png",
+    techName: "GIT",
+  },
+
+];
