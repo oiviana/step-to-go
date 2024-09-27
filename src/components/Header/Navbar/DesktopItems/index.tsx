41d0d652b9abf30637947d054b798ad5a549fc95
oiviana <vianalucas80@gmail.com>
2024-09-27 16:16:43 -0300

    feat: header mobile structure

--- src/components/Header/Navbar/DesktopItems/index.tsx
+++ src/components/Header/Navbar/DesktopItems/index.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import { useNavigationPage } from "@/utils/Providers";
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 export default function DesktopItems() {
-
+   const translate = useTranslations("Header");
     const {setPage, page} = useNavigationPage();
     const [activeSection, setActiveSection] = useState("");
 
@@ -14,14 +15,18 @@ export default function DesktopItems() {
     }
 
   return (
-    <ul className="hidden lg:flex text-v-white-500 items-center gap-8 md:text-lg lg:gap-20 font-semibold text-base">
+    <ul className="hidden lg:flex text-v-white-500 items-center gap-6 md:text-base lg:gap-14 font-semibold text-base lg:mr-6">
       <li className={`transition-colors cursor-pointer  ${activeSection === 'about' && activeSection === page && 'text-v-green'} min-w-[104px]`}
       onClick={() => handleSetSection("about")}>
-        Carreira
+         {translate(`navbar.0`)}
       </li>
       <li className={`transition-colors cursor-pointer  ${activeSection === 'projects' && activeSection === page && 'text-v-green'} min-w-[104px]`}
        onClick={() => handleSetSection("projects")}>
-        Projetos
+         {translate(`navbar.1`)}
+      </li>
+      <li className={`transition-colors cursor-pointer  ${activeSection === 'projects' && activeSection === page && 'text-v-green'} min-w-[104px]`}
+       onClick={() => handleSetSection("projects")}>
+        {translate(`navbar.2`)}
       </li>
     </ul>
   );
--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useTranslations } from "next-intl";
 import TranslateButton from "../../ui/TranslateButton";
 import DesktopItems from "./DesktopItems";
+import { useNavigationPage } from "@/utils/Providers";
 
 type NavbarProps = {
   showMobileMenu: boolean
@@ -11,20 +12,31 @@ type NavbarProps = {
 }
 
 export default function Navbar({showMobileMenu, setShowMenu}:NavbarProps) {
-
+  const {setPage} = useNavigationPage();
   const translate = useTranslations("Header");
   return (
-    <nav className="flex w-full max-w-[1440px] mx-auto">
+    <nav className="flex w-full max-w-[450px]">
       <ul
         className={`${
           showMobileMenu ? "fixed z-50" : "hidden"
         } lg:hidden inset-y-0 left-0 top-16 w-full overflow-y-auto bg-v-dark-test flex flex-col gap-7 text-v-white-500 text-base px-3 pt-9`}
       >
         <div className="flex flex-col gap-10 ml-3">
-          <li>
-            <Link href="/#about" onClick={() => setShowMenu(false)}>
-              {translate(`navbar.0`)}
-            </Link>
+          <li
+          onClick={() => {
+            setPage('about')
+            setShowMenu(false)
+          }}
+          >
+            {translate(`navbar.0`)}
+          </li>
+          <li
+          onClick={() => {
+            setPage('projects')
+            setShowMenu(false)
+          }}
+          >
+            {translate(`navbar.1`)}
           </li>
           <div className="flex gap-2">
             <TranslateButton />
