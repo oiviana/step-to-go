64f7b8fc8c99fdb9cad42a5abfa51d76707da3a2
oiviana <vianalucas80@gmail.com>
2024-10-02 17:26:22 -0300

    refactor: new image gallery

--- src/components/ProjectDetails/ProjectSlider/index.tsx
+++ src/components/ProjectDetails/ProjectSlider/index.tsx
@@ -1,7 +1,8 @@
-"use client";
+
 import React from "react";
 import { ContentImage } from "@/contentful/contentImage";
 import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+import useScreenSize from "@/hooks/useScreenSize";
 
 interface ProjectSliderProps {
   images: ContentImage[] | null;
@@ -9,23 +10,26 @@ interface ProjectSliderProps {
 }
 
 export default function ProjectSlider({ images, mobileImages }: ProjectSliderProps) {
+
+  const deviceType = useScreenSize();
+
   const convertToGalleryItems = (imageList: ContentImage[] | null): ReactImageGalleryItem[] => {
     if (!imageList) return [];
-  
+
     return imageList.map(image => ({
       original: image.src,
       originalAlt: image.alt,
-      thumbnail: image.src,  
+      thumbnail: image.src,
       originalTitle: image.alt,
-    
+
     }));
   };
 
 
   return (
     <>
-    <ImageGallery items={convertToGalleryItems(images)}
-    showPlayButton={false}/>;
+      <ImageGallery items={deviceType === 'desktop' ? convertToGalleryItems(images) : convertToGalleryItems(mobileImages)}
+        showPlayButton={false} />
     </>
   );
 }
