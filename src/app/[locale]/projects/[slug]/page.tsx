be7b58a024e370cf1b8c2ce4ed0687d42874f985
oiviana <vianalucas80@gmail.com>
2024-10-02 17:25:59 -0300

    refactor: new project page

--- src/app/[locale]/projects/[slug]/page.tsx
+++ src/app/[locale]/projects/[slug]/page.tsx
@@ -29,9 +29,6 @@ export default function ProjectPage({ params }: ProjectPageProps) {
     fetchData();
   }, [locale, params.slug]);
 
-
-
-  const stackList = project?.technologies.join(" - ");
   const translate = useTranslations("ProjectDetails");
 
   if (!params.slug || !project) {
@@ -40,7 +37,7 @@ export default function ProjectPage({ params }: ProjectPageProps) {
 
   return (
     <div className="max-w-[1210px] mx-auto px-2 flex flex-col gap-4">
-      <header className="flex py-3 shadow-xl mt-3 px-2 dark:text-v-white-300">
+      <header className="flex py-3  mt-3 px-2 text-v-purple-light border-b-2 border-v-dark-900">
         <Link
           href="/"
           className="flex justify-between p-2 gap-2 font-bold text-lg "
@@ -52,38 +49,40 @@ export default function ProjectPage({ params }: ProjectPageProps) {
           {translate("title")}
         </h2>
       </header>
-      <div className="lg:flex lg:gap-2">
-      <section className="mb-5 ">
-        <div className="h-[400px] lg:h-[800px] w-full">
+      <div className="flex flex-col lg:flex-row lg:gap-2">
+      <section className="h-auto">
           <ProjectSlider images={project.images} mobileImages={project.mobileImages} />
-        </div>
       </section>
       <section className="lg:max-w-[410px]">
-        <div className="flex flex-col shadow-lg py-6 md:p-6 gap-4 w-full  px-2 text-v-white-300">
+        <div className="flex flex-col  py-6 md:p-6 gap-4 w-full  px-2 text-v-white-300">
           <div className="flex justify-center md:justify-between flex-col md:flex-row">
-            <h1 className="text-xl md:text-2xl font-bold text-center lg:text-left">
+            <h1 className="text-lg md:text-xl font-bold text-center lg:text-left text-v-green mb-4">
               {project.title}
             </h1>
           </div>
-          <span className="text-slate-600 dark:text-slate-500">
-            {translate("tech")} {stackList}
-          </span>
-          <div className="flex gap-4 justify-center mt-4 md:mt-0">
-              <button className="flex bg-v-dark-bold dark:bg-v-white-500 dark:text-v-dark-bold text-v-white-300 text-lg font-semibold py-2 px-3 gap-3 items-center rounded">
+          <div className="flex gap-3 flex-wrap justify-center lg:justify-start">
+          {project.technologies.map((item, index) => (
+            <span className="text-xs md:text-sm text-v-purple-light font-semibold border-[2px] border-v-purple-light rounded-md p-1 cursor-default" key={index}>
+              {item}
+            </span>
+          ))}
+          </div>
+          <article className="mt-4 !leading-[40px] text-sm md:text-md !font-normal lg:text-justify mb-4">
+            <b className="text-v-green">{translate("description")} </b>
+            {project.description}
+          </article>
+          <div className="flex gap-4 justify-center mt-4 md:mt-0 lg:justify-start">
+              <button className="flex w-32 justify-center bg-v-green hover:bg-[#b3eb7a] transition-colors text-v-dark-test text-base font-semibold py-2 px-3 gap-3 items-center rounded">
                 <FaCode size={18} />
                 <a href={project.codeUrl} target="_blank">{translate("codebutton")}</a>
               </button>
               {project?.hasDeploy && (
-                <button className="flex bg-v-dark-bold dark:bg-v-white-500 dark:text-v-dark-bold text-v-white-300 text-lg font-semibold py-2 px-3 gap-3 items-center rounded">
+                <button className="flex w-32 justify-center bg-v-green hover:bg-[#b3eb7a] transition-colors text-v-dark-test text-base font-semibold py-2 px-3 gap-3 items-center rounded ">
                   <VscPreview size={18} />
                   <a href={project.deployUrl} target="_blank">Deploy</a>
                 </button>
               )}
             </div>
-          <article className="mt-8 !leading-[40px] text-sm md:text-md !font-normal">
-            <b>{translate("description")} </b>
-            {project.description}
-          </article>
         </div>
       </section>
       </div>
