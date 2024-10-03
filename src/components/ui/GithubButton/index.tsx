f6bede0fdc52dfc82b36991ca220401b3ed5200f
oiviana <vianalucas80@gmail.com>
2024-10-03 13:40:36 -0300

    fix: acessibility links

--- src/components/ui/GithubButton/index.tsx
+++ src/components/ui/GithubButton/index.tsx
@@ -5,6 +5,7 @@ export default function GithubButton(){
     return(
         <a className="cursor-pointer"
         target="_blank"
+         aria-label="Perfil de Lucas Viana no Github"
         href="https://github.com/oiviana">
             <FaGithub size={25} className="text-v-green hover:text-[#b3eb7a] transition-colors"/>
         </a>
--- src/components/ui/LinkedinButton/index.tsx
+++ src/components/ui/LinkedinButton/index.tsx
@@ -5,6 +5,7 @@ export default function LinkedinButton(){
     return(
         <a className="cursor-pointer"
         target="_blank"
+         aria-label="Perfil de Lucas Viana no Linkedin"
         href="https://www.linkedin.com/in/lucasviana80/">
             <FaLinkedin size={25} className="text-v-green hover:text-[#b3eb7a] transition-colors"/>
         </a>
