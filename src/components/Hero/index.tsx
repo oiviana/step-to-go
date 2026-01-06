f4c73c0bc8b000728f0a309c1d5f30fdd5f12ae2
oiviana <vianalucas80@gmail.com>
2026-01-06 15:44:01 -0300

    feat: cv translated

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
             {translate("description")}
           </p>
           <a
-            href={process.env.CV_BUTTON_URL ?? 'https://drive.google.com/file/d/1BAALqxLvbuSeDMygtVv3v0mdtl3Vf6ID/view?usp=sharing'}
+            href={translate("link")}
             target="_blank"
             className="green-light w-[214px] p-3 h-[60px] flex justify-center gap-2 items-center mt-3 rounded-md  shadow-md font-semibold bg-transparent text-v-green border-2 border-v-green transition-colors"
           >
